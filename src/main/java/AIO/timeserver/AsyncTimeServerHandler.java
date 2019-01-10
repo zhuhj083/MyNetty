87431b316aa747d41d8bc1c95bdc092608597b52
zhuhaijun <apple@zhuhaijundeMacBook-Pro.local>
2019-01-10 15:55:31 +0800

    creat MyNetty

---
+++ src/main/java/AIO/timeserver/AsyncTimeServerHandler.java
@@ -0,0 +1,39 @@
+package AIO.timeserver;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.util.concurrent.CountDownLatch;
+
+public class AsyncTimeServerHandler implements  Runnable {
+
+    private int port;
+    CountDownLatch latch;
+    AsynchronousServerSocketChannel asynchronousServerSocketChannel;
+
+    public AsyncTimeServerHandler(int port) {
+        this.port = port;
+        try{
+            asynchronousServerSocketChannel = AsynchronousServerSocketChannel.open();
+            asynchronousServerSocketChannel.bind(new InetSocketAddress(port));
+            System.out.println("The time server is start in port:"+port);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        latch = new CountDownLatch(1);
+        doAccept();
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void doAccept(){
+        asynchronousServerSocketChannel.accept(this,new AcceptCompletionHandler());
+    }
+}
